1036d9051d02c4413909d9d85c56465921c1a218
Zach Johnson <me@zajohnson.com>
2015-06-03 18:43:59 -0700

    Tweak some numbers

--- child-pages.php
+++ child-pages.php
@@ -4,7 +4,7 @@ Adds Child_Pages_Widget widget.
 Plugin Name:  WP Child Pages
 Plugin URI: http://lefkomedia.com
 Description: Lists out the child pages.
-Version: 1.1.0
+Version: 1.1.1
 Author: Lefko Media
 Author URI: https://github.com/lefkomedia/wp-child-pages
 */
@@ -130,14 +130,14 @@ function github_plugin_updater_test_init() {
 	if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
 		$config = array(
 			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-			'proper_folder_name' => 'child-pages', // this is the name of the folder your plugin lives in
+			'proper_folder_name' => 'wp-child-pages', // this is the name of the folder your plugin lives in
 			'api_url' => 'https://api.github.com/repos/lefkomedia/wp-child-pages', // the github API url of your github repo
 			'raw_url' => 'https://raw.github.com/lefkomedia/wp-child-pages/master', // the github raw url of your github repo
 			'github_url' => 'https://github.com/lefkomedia/wp-child-pages', // the github url of your github repo
 			'zip_url' => 'https://github.com/lefkomedia/wp-child-pages/zipball/master', // the zip url of the github repo
 			'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 			'requires' => '3.0', // which version of WordPress does your plugin require?
-			'tested' => '3.9', // which version of WordPress is your plugin tested up to?
+			'tested' => '4.2.2', // which version of WordPress is your plugin tested up to?
 			'readme' => 'README.md', // which file to use as the readme for the version number
 			'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
 		);
